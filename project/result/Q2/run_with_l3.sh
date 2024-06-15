00d9ac706120f0994b518bf47d475a9e3a40298a
RogelioKG <rogeliokg0813@gmail.com>
2024-06-16 01:38:49 +0800

    Change result/Q2 dir structure

--- project/result/Q2/run_with_l3.sh
+++ project/result/Q2/run_with_l3.sh
@@ -1,4 +1,6 @@
 PROJECT_DIR="${HOME}/Gem5-Practice/project"
+WORKING_DIR="${PROJECT_DIR}/result/Q2/enable_l3"
+
 OPTIONS="\
  --cmd ${PROJECT_DIR}/gem5/tests/test-progs/hello/bin/x86/linux/hello\
  --cpu-type=TimingSimpleCPU\
@@ -6,14 +8,14 @@ OPTIONS="\
  --l1i_size=32kB --l1d_size=32kB --l2_size=128kB --l3_size=1MB\
  --mem-type=NVMainMemory\
  --nvmain-config=${PROJECT_DIR}/NVmain/Config/PCM_ISSCC_2012_4GB.config\
- --output=${PROJECT_DIR}/result/Q2/std_output.txt\
+ --output=${WORKING_DIR}/std_output.txt\
 "
 
 # m5out will be generated in working dir
-pushd ${PROJECT_DIR}/result/Q2
+pushd ${WORKING_DIR}
 
 # command
-${PROJECT_DIR}/gem5/build/X86/gem5.opt ${PROJECT_DIR}/gem5/configs/example/se.py ${OPTIONS} > ${PROJECT_DIR}/result/Q2/cmd_output.txt
+${PROJECT_DIR}/gem5/build/X86/gem5.opt ${PROJECT_DIR}/gem5/configs/example/se.py ${OPTIONS} > ${WORKING_DIR}/cmd_output.txt
 
 # back to original dir
 popd
